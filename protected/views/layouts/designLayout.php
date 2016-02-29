c8c5d66a414a8b9a3d5e5a04ccad1fd0495c10ed
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2016-02-29 21:31:07 +0300

    Added link on the catalog button. Separated main_buttons from the layout into //layouts/_main_layout.php file.

--- protected/views/layouts/designLayout.php
+++ protected/views/layouts/designLayout.php
@@ -143,31 +143,7 @@
             </ul>
         </div>
         <div class="clear"></div>
-        <div class="info">
-            <div>
-                <h3>КАТАЛОГ</h3>
-                <p>Врачи, Клиники, Диагностические центры – поиск по параметрам
-                </p>
-            </div>
-            <div onClick="location.href='<?php echo Yii::app() -> baseUrl;?>/rating'">
-                <h3>РЕЙТИНГ</h3>
-                <p>Медицинские центры, отсортированные по рейтингу на основе отзывов лечащих врачей и пациентов
-                </p>
-            </div>
-            <div>
-                <h3>АКЦИИ И СКИДКИ</h3>
-                <p>Информация о действующих Акциях и Скидках в некоторых Клиниках и Диагностических центрах</p>
-            </div>
-            <div onClick="location.href='<?php echo Yii::app() -> baseUrl;?>/about'">
-                <h3>О НАС</h3>
-                <p>
-                </p>
-            </div>
-            <div onclick="$('#exampleModal1').arcticmodal()">
-                <h3>МРТ И КТ</h3>
-                <p style="font-size:10px;">Для получения консультации специалиста по вопросам МРТ и КТ диагностики оставьте свой номер телефона и ФИО и Вам перезвонят.</p>
-            </div>
-        </div>
+        <?php $this -> renderPartial('//layouts/_main_buttons'); ?>
         <div class="clear"></div>
 
         <div class="main_content">
