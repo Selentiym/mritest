4710484535abc60be9625aedba16fb6ecb53f7d1
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2016-08-30 17:52:21 +0300

    Changed yandex counter

--- protected/views/layouts/designLayout.php
+++ protected/views/layouts/designLayout.php
@@ -58,42 +58,35 @@
 		ga('send', 'pageview');
 
 	</script>
-	<!-- Yandex.Metrika counter -->
-	<script type="text/javascript">
-	(function (d, w, c) {
-		(w[c] = w[c] || []).push(function() {
-			try {
-				w.yaCounter34319460 = new Ya.Metrika({id:34319460,
-						webvisor:true,
-						clickmap:true,
-						trackLinks:true,
-						accurateTrackBounce:true});
-			} catch(e) { }
-		});
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter39271440 = new Ya.Metrika({
+                        id:39271440,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
+            });
 
-		var n = d.getElementsByTagName("script")[0],
-			s = d.createElement("script"),
-			f = function () { n.parentNode.insertBefore(s, n); };
-		s.type = "text/javascript";
-		s.async = true;
-		s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
 
-		if (w.opera == "[object Opera]") {
-			d.addEventListener("DOMContentLoaded", f, false);
-		} else { f(); }
-	})(document, window, "yandex_metrika_callbacks");
-	</script>
-	<noscript><div><img src="//mc.yandex.ru/watch/34319460" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	<style>
-		.info div{
-			cursor:pointer;
-		}
-		.info div h3 {
-			margin:0px auto;
-			padding:5px 0 0 0;
-		}
-	</style>
-	<!-- /Yandex.Metrika counter -->
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/39271440" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
     <div class="wrapper">
